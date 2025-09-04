65d0c8a0fa95722b135e4584972640ef504a38dc
Thomas Blank <thomi.blank@gmail.com>
2025-09-04 13:50:52 +0000

    Add resume data

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,237 +1,138 @@
 import type { ResumeData } from "@/lib/types";
 
 export const RESUME_DATA: ResumeData = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
-  about: "Full Stack Engineer dedicated to building high-quality products.",
+  name: "Thomas Blank",
+  initials: "TB",
+  location: "Freiburg, Germany, CET",
+  locationLink: "https://www.google.com/maps/place/Freiburg",
+  about: "Senior Software Engineer for Machine Learning",
   summary: (
     <>
-      Full Stack Engineer specializing in high-performance React applications,
-      scalable Node.js services, and real-time collaboration systems.
-      Experienced in technical architecture design and remote team leadership.
+      Accomplished software engineer with extensive experience in developing and optimizing advanced AI frameworks and training workloads.
     </>
   ),
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+  avatarUrl: "https://avatars.githubusercontent.com/u/4013007?v=4",
+  personalWebsiteUrl: "https://aboutblank.me",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "thomi.blank@gmail.com",
+    tel: "",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/thoomi",
         icon: "github",
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/blanktho/",
         icon: "linkedin",
-      },
-      {
-        name: "X",
-        url: "https://x.com/BartoszJarocki",
-        icon: "x",
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Master's Degree in Applied Computer Science, FH Erfurt",
+      degree: "Thesis: Machine Learning based analysis of complex sign plates (object detection & semantic segmentation)",
+      start: "2014",
+      end: "2018",
+    },
+    {
+      school: "Bachelor' Degree in Applied Computer Science, FH Erfurt",
+      degree: "",
+      start: "2011",
+      end: "2014",
     },
   ],
   work: [
     {
-      company: "Motion",
-      link: "https://motionapp.com/",
-      badges: ["Remote", "AI", "React", "Next.js", "TypeScript", "AdonisJS"],
-      title: "Senior Software Engineer",
-      start: "2025",
-      end: null,
-      description: (
-        <>
-          Working on internal AI agents platform allowing marketing specialists
-          to create AI workflows.
-        </>
-      ),
-    },
-    {
-      company: "Film.io",
-      link: "https://film.io",
-      badges: ["Remote", "React", "Next.js", "TypeScript", "Node.js"],
-      title: "Software Architect",
-      start: "2024",
+      company: "Bosch, Level 4 driving project",
+      link: "https://www.bosch.com/stories/topics/automated-driving/",
+      badges: [],
+      title: "Senior Software Engineer - Machine Learning",
+      start: "2021",
       end: "2025",
-      description: (
+      description:  (
         <>
-          Leading technical architecture of a blockchain-based film funding
-          platform.
           <ul className="list-inside list-disc">
             <li>
-              Architecting migration from CRA to Next.js for improved
-              performance, SEO, and DX
+              Transformed the stack from single-camera networks to spatio-temporal multi-view networks
             </li>
             <li>
-              Established release process enabling faster deployments and
-              reliable rollbacks
+              Close collaboration with corporate research
             </li>
             <li>
-              Implementing system-wide monitoring and security improvements
+              Successful customer demos of urban hands-free driving using solely cameras (vision only)
+            </li>
+            <li>
+              Software framework that significantly improved the training efficiency of a single multi-view, multi-task network on diverse and task-specific data, including seamless integration of temporal data
+            </li>
+            <li>
+              Training optimization for A100 yielding 7-figure savings YoY
             </li>
           </ul>
         </>
       ),
     },
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: [
-        "Remote",
-        "React",
-        "TypeScript",
-        "Node.js",
-        "GraphQL",
-        "Tailwind CSS",
-      ],
-      title: "Senior Full Stack Developer",
+      company: "Bosch & Daimler Collaboration Project Athena",
+      link: "https://www.businessinsider.de/gruenderszene/allgemein/daimler-bosch-roboter-taxis/",
+      badges: [],
+      title: "Software Engineer - Machine Learning",
+      start: "2018",
+      end: "2021",
+      description: (
+        <>
+          <ul className="list-inside list-disc">
+            <li>
+              Deep Learning based camera perception for automated driving
+            </li>
+            <li>
+              Multi Task CNNs (Semantic Segmentation, 3d Object Detection, Object Tracking, …)
+            </li>
+            <li>
+              Co-Creator xflow: reusable training modules to facilitate collaboration in teams {">"} 50 devs
+            </li>
+            <li>
+              Continuous & reproducible data parallel trainings on AzureML
+            </li>
+            <li>
+              Real-time optimization & automatic deployment into the car (TensorRT, ONNX)
+            </li>
+          </ul>
+        </>
+      ),
+    },
+    {
+      company: "YAZIO",
+      link: "https://www.yazio.com",
+      badges: [],
+      title: "Full Stack Web Developer",
       start: "2021",
       end: "2024",
       description: (
         <>
-          Senior developer and squad leader for an enterprise agile meeting
-          platform.
           <ul className="list-inside list-disc">
             <li>
-              Built design system with Tailwind CSS, improving development speed
-              and time to market
+              Successfully developed a high performance & responsive website, seen by over a million people per month
             </li>
             <li>
-              Implemented engineering practices: PR automation, code review
-              guidelines, and workflows
-            </li>
-            <li>
-              Open source contributions to Relay DevTools and React i18n tooling
+              Built a scalable and resilient Symfony2 REST API, served by Nginx and enhanced with a Redis caching layer, using Ansible for efficient server and development environment management
             </li>
           </ul>
         </>
       ),
-    },
-    {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote", "React", "TypeScript", "Node.js", "Android", "Kotlin"],
-      title: "Lead Android Developer → Full Stack Developer",
-      start: "2015",
-      end: "2021",
-      description: (
-        <>
-          Successfully transitioned from mobile to full-stack development while
-          leading distributed teams.
-          <ul className="list-inside list-disc">
-            <li>
-              Led frontend team at Evercast, building real-time platform
-              supporting 30+ users per room with HD streaming and collaboration
-              tools
-            </li>
-            <li>
-              Developed offline-first Android app for DKMS, improving donor
-              registration process
-            </li>
-            <li>
-              Led development teams across multiple successful client projects
-            </li>
-          </ul>
-        </>
-      ),
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: ["On Site", "Android", "Java", "Kotlin"],
-      title: "Android Developer → Lead Android Developer",
-      start: "2012",
-      end: "2015",
-      description: (
-        <>
-          First Android developer, grew and led a team of 15+ engineers while
-          establishing engineering culture.
-          <ul className="list-inside list-disc">
-            <li>
-              Developed apps for major Polish companies including LOT, Polskie
-              Radio, and Agora
-            </li>
-            <li>Built and mentored high-performing mobile development team</li>
-          </ul>
-        </>
-      ),
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: ["On Site", "C/C++", "LTE", "Agile"],
-      title: "C/C++ Developer",
-      start: "2010",
-      end: "2012",
-      description:
-        "Developed software for LTE base stations at enterprise scale, gaining strong fundamentals in software architecture, testing practices, and cross-team collaboration.",
     },
   ],
   skills: [
-    "React/Next.js/Remix",
-    "TypeScript",
-    "Tailwind CSS",
-    "Design Systems",
-    "WebRTC",
-    "WebSockets",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "System Architecture",
-    "Remote Team Leadership",
+    "Torch",
+    "Tensorflow",
+    "Python",
+    "AzureML & DevOps",
+    "Model deployment",
+    "Object Detection & Tracking",
+    "CUDA",
+    "Training optimization",
   ],
   projects: [
-    {
-      title: "Monito",
-      techStack: ["TypeScript", "Next.js", "Browser Extension", "PostgreSQL"],
-      description:
-        "Browser extension for debugging web applications. Includes taking screenshots, screen recording, E2E tests generation and generating bug reports",
-      link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
-      },
-    },
-    {
-      title: "Consultly",
-      techStack: [
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
-        "Tailwind CSS",
-        "PostgreSQL",
-        "Redis",
-      ],
-      description:
-        "Platform for online consultations with real-time video meetings and scheduling",
-      link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
-      },
-    },
-    {
-      title: "Minimalist CV",
-      techStack: ["TypeScript", "Next.js", "Tailwind CSS"],
-      description:
-        "An open source minimalist, print friendly CV template with a focus on readability and clean design. >9k stars on GitHub",
-      link: {
-        label: "Minimalist CV",
-        href: "https://github.com/BartoszJarocki/cv",
-      },
-    },
   ],
 } as const;
